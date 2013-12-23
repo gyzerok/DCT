5f75f44943f93c4e311181d27f82011fd0713c18
zuiris <siriuz1g@gmail.com>
2013-12-23 16:57:56 +0600

    quantizer implemented

---
+++ DCT/Quantizer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DCT
+{
+    class Quantizer
+    {
+        private int[,] qualityMatrix = new int[8,8];
+        private int quality;
+
+        public Quantizer(int quality)
+        {
+            this.quality = quality;
+            this.MakeQualityMatrix();
+        }
+
+        private void MakeQualityMatrix()
+        {
+            for (var i = 0; i < qualityMatrix.GetLength(1); i++)
+            {
+                for (var j = 0; j < qualityMatrix.GetLength(0); j++)
+                {
+                    qualityMatrix[i, j] = 1 + ((1 + i + j)*this.quality);
+                }
+            }
+        }
+
+        public int[,] MakeQuantization(int[,] matrix)
+        {
+            var resultMatrix = new int[8,8];
+
+            for (var i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (var j = 0; j < matrix.GetLength(0); j++)
+                {
+                    var temp = (int)Math.Round((double)matrix[i, j]/(double)qualityMatrix[i, j]);
+                    resultMatrix[i,j] = temp;
+                }
+            }
+            return this.qualityMatrix;
+        }
+    }
+}
