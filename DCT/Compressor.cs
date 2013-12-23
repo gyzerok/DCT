d8df0b4283d6e323fec1deb1e8238d2d01865ea6
zuiris <siriuz1g@gmail.com>
2013-12-23 17:30:19 +0600

    compressor implemented

---
+++ DCT/Compressor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCT
+{
+    public class Compressor
+    {
+        /// <summary>
+        /// Static method that compresses data using zero coding
+        /// </summary>
+        /// <param name="toCompressList"></param>
+        /// <returns></returns>
+        public static List<Tuple<int, int>> MakeCompression(List<int> toCompressList)
+        {
+            var result = new List<Tuple<int, int>>();
+            var zeroCounter = 0;
+
+            foreach (var value in toCompressList)
+            {
+                if (value == 0)
+                {
+                    zeroCounter++;
+                }
+                else
+                {
+                    result.Add(new Tuple<int, int>(zeroCounter, value));
+                    zeroCounter = 0;
+                }
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Static method that decompresses data using zero coding
+        /// </summary>
+        /// <param name="toDecompressList"></param>
+        /// <returns></returns>
+        public static List<int> MakeDecompression(List<Tuple<int, int>> toDecompressList)
+        {
+            var result = new List<int>();
+
+            foreach (var value in toDecompressList)
+            {
+                for (var i = 0; i<value.Item1; i++)
+                {
+                    result.Add(0);
+                }
+                
+                result.Add(value.Item2);
+            }
+
+            return result;
+        }
+    }
+}
