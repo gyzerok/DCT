945dd22e255785aedc2755c77e99145b4f3084fb
gyzerok <gyzerok@gmail.com>
2013-12-23 17:31:50 +0700

    Classes for YUV implemented

---
+++ DCT/ColorYUV.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace DCT
+{
+    public class ColorYUV
+    {
+        public double Y { get; set; }
+        public double U { get; set; }
+        public double V { get; set; }
+
+        public static ColorYUV FromColor(Color color)
+        {
+            ColorYUV yuvColor = new ColorYUV();
+
+            yuvColor.Y = 0.299 * color.R + 0.587 * color.G + 0.114 * color.B;
+            yuvColor.U = color.B - yuvColor.Y;
+            yuvColor.V = color.R - yuvColor.Y;
+
+            return yuvColor;
+        }
+
+        public Color ToColor()
+        {
+            int R, G, B;
+
+            R = (int)(this.Y + 1.13983 * (this.V - 128));
+            G = (int)(this.Y - 0.39465 * (this.U - 128) - 0.58060 * (this.V - 128));
+            B = (int)(this.Y + 2.03211 * (this.U - 128));
+
+            R = R > 255 ? 255 : R < 0 ? 0 : R;
+            G = G > 255 ? 255 : G < 0 ? 0 : G;
+            B = B > 255 ? 255 : B < 0 ? 0 : B;
+
+            return Color.FromArgb(R, G, B);
+        }
+    }
+}
