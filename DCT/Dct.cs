62d0d696ff31da9830bb9e929d31e9ada96a1568
gyzerok <gyzerok@gmail.com>
2014-01-05 18:28:52 +0700

    decompress method

--- DCT/Dct.cs
+++ DCT/Dct.cs
@@ -15,15 +15,17 @@ namespace DCT
             this.quantizer = new Quantizer(quantity);
         }
 
-        public List<List<int>> Compress(Matrix p)
+        public List<List<int>> Compress(Matrix matrix)
         {
-            var pDct = this.dctMatrix * p * this.dctMatrix.Transpose;
-
             // Формируем подматрицы 8х8
             var submatrixList = new List<Matrix>();
-            for (int i = 0; i < p.Rows; i = i + 8)
-                for(int j = 0; j < p.Cols; j = j + 8)
-                    submatrixList.Add(p.Submatrix(i, j, 8, 8));
+            for (int i = 0; i < matrix.Rows; i = i + 8)
+                for (int j = 0; j < matrix.Cols; j = j + 8)
+                    submatrixList.Add(matrix.Submatrix(i, j, 8, 8));
+
+            // Применяем ДКТ
+            for (int i = 0; i < submatrixList.Count; i++)
+                submatrixList[i] = this.dctMatrix * submatrixList[i] * this.dctMatrix.Transpose;
 
             // Квантуем все подматрицы
             for (int i = 0; i < submatrixList.Count; i++)
@@ -39,7 +41,15 @@ namespace DCT
 
         public void Decompress(List<List<int>> vectorsList)
         {
-            
+            var submatrixList = new List<Matrix>();
+            for (int i = 0; i < vectorsList.Count; i++)
+                submatrixList.Add(ZigzagScan.ReverseScan(vectorsList[i]));
+
+            for (int i = 0; i < submatrixList.Count; i++)
+                submatrixList[i] = this.quantizer.Dequantize(submatrixList[i]);
+
+            for (int i = 0; i < submatrixList.Count; i++)
+                submatrixList[i] = this.dctMatrix * submatrixList[i] * this.dctMatrix.Transpose;
         }
 
         public Matrix Generate()
